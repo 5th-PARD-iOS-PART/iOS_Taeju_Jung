029a2d2839641184f7d9f7bf8938b86ad8f255e3
Jeong Taeju <taeju0627@gmail.com>
2025-03-28 13:07:39 +0900

    WTP: test

---
+++ 2nd_HW2_TaejuJung/2nd_HW2_TaejuJung/_nd_HW2_TaejuJungApp.swift
@@ -0,0 +1,17 @@
+//
+//  _nd_HW2_TaejuJungApp.swift
+//  2nd_HW2_TaejuJung
+//
+//  Created by 정태주 on 3/28/25.
+//
+
+import SwiftUI
+
+@main
+struct _nd_HW2_TaejuJungApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
